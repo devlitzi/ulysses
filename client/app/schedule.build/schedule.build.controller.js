8ea549a0161147ffe827afa8c4aec537ac5083da
Zach Litzinger <litzi005@morris.umn.edu>
2016-04-26 12:54:31 -0500

    adding date input, need to wire it up (Sean and Zach)

--- client/app/schedule.build/schedule.build.controller.js
+++ client/app/schedule.build/schedule.build.controller.js
@@ -3,6 +3,7 @@
 angular.module('ulyssesApp')
   .controller('ScheduleBuildCtrl', function($scope) {
     $scope.schedule = null;
+    $scope.blindDate = false;
 
     $scope.$parent.schedule.$promise.then(function(schedule) {
       $scope.schedule = schedule;
@@ -22,5 +23,10 @@ angular.module('ulyssesApp')
 
     $scope.removeSlot = function(job, index) {
       job.slots.splice(index, 1);
+    };
+
+    $scope.blindify = function() {
+      console.log("blind date?" + $scope.blindDate);
+      $scope.blindDate = !$scope.blindDate;
     };
   });
