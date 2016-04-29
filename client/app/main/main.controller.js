b2dc05ad04d9ebe9fa61b142216201b80ec14b0f
Zach Litzinger <litzi005@morris.umn.edu>
2016-04-28 23:48:55 -0500

    can create a new schedulegit status! Closes #26

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,10 +3,15 @@
 angular.module('ulyssesApp')
   .controller('MainController', function ($scope, $state, Schedule, $stateParams) {
     $scope.schedule = Schedule.get($stateParams);
+    $scope.schedules = Schedule.query();
 
-    $scope.makesSchedule = function(){
-  
-    };
+    $scope.addSchedule = function() {
+      if($scope.scheduleName.length > 0) {
+        Schedule.save({name: $scope.scheduleName});
+        $scope.schedules = Schedule.query();
+        $scope.scheduleName = "";
+        location.reload(true);
+      }
+    }
 
   });
-
