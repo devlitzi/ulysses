689198cf88816246d46cf48ba2b6dfaf1caab2d6
devlitz <z_wolfe@mac.com>
2016-05-06 10:59:54 -0500

    delete works, yay, life, woo! Closes #32

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,12 +4,23 @@ class NavbarController {
   isCollapsed = true;
   scheduleDropdown = false;
 
-  constructor($scope, Auth, Schedule) {
+  constructor($scope, Auth, Schedule, $rootScope, $location) {
     this.isLoggedIn = Auth.isLoggedIn;
     this.isAdmin = Auth.isAdmin;
     this.hasRole = Auth.hasRole;
     this.getCurrentUser = Auth.getCurrentUser;
     $scope.schedules = Schedule.query();
+
+      //deletes the schedule and takes user to the main page
+      $rootScope.deleteSchedule = function(schedule) {
+          if (confirm("Are you SURE you want to delete the schedule?")) {
+            Schedule.remove({id: schedule._id}).$promise.then(function() {
+              $scope.schedules = Schedule.query();
+              $location.path('main');
+            });
+          }
+        };
+
   }
 }
 
